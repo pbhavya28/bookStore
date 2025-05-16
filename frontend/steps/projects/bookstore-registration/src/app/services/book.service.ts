a3997ebce74084dc1a33d3073d1a230fdbf2aac2
Indrajeet Khatri <indrajeet.khatri@valtech.com>
2025-05-16 17:02:56 +0530

    added the admin registration functionality

--- frontend/steps/projects/bookstore-registration/src/app/services/book.service.ts
+++ frontend/steps/projects/bookstore-registration/src/app/services/book.service.ts
@@ -6,10 +6,19 @@ import { Observable } from 'rxjs';
 export class BookService {
   private baseUrl = 'http://localhost:3000/api/books';
 
+  bookData:any = []
   constructor(private http: HttpClient) {}
 
   getBookById(id: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/${id}`);  
+  }
+
+  getBooks(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  deleteBook(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
   }
 
   rateBook(id: string, rating: number, comment: string): Observable<any> {
