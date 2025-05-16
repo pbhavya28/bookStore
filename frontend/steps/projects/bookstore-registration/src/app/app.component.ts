b98e2ec10e1e9d253b6e44263a4255920cfd5c5e
Bhavya ileshkumar <bhavya.ileshkumar@valtech.com>
2025-05-16 15:14:54 +0530

    added changes

--- frontend/steps/projects/bookstore-registration/src/app/app.component.ts
+++ frontend/steps/projects/bookstore-registration/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-root',
 
-  template: `
-    <!-- <app-detail></app-detail> -->
+  template: `     
     <router-outlet></router-outlet>
   `,
   standalone: false,
